ffcafabed30363f3740b965f80771d2e146c17d8
jbconway <jbconway0215@gmail.com>
2025-07-21 15:19:00 -0400

    updates to main.py

--- src/main.py
+++ src/main.py
@@ -14,17 +14,24 @@ import calibration_plot
 def main():
 
     # PART 1: Instanciate etl, saving the two datasets in `./data/`
-    pred_universe_raw, arrest_events_raw = etl.etl()
-    print(pred_universe_raw.head()) 
-    print(arrest_events_raw.head())
+    etl.etl()
+    print("ETL completed. Data saved in `data/` directory.")
+    # print(pred_universe_raw.head()) 
+    # print(arrest_events_raw.head())
 
     # PART 2: Call functions/instanciate objects from preprocessing
+    df_arrests = preprocessing.preprocessing()
+    print("Preprocessing completed.")
+    # print(df_arrests.head())
 
     # PART 3: Call functions/instanciate objects from logistic_regression
+    df_arrests_train, df_arrests_test, = logistic_regression.logistic_regression(df_arrests)
 
     # PART 4: Call functions/instanciate objects from decision_tree
+    df_arrests_test = decision_tree.decision_tree(df_arrests_train, df_arrests_test)
 
     # PART 5: Call functions/instanciate objects from calibration_plot
+    calibration_plot.calibration_plot_main(df_arrests_test)
 
 
 if __name__ == "__main__":
